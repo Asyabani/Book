7c782aaf75338f530c6f2fcb6dd653520906c7a2
nurzaman asyabani <nurzamanasya@gmail.com>
2024-07-10 21:22:17 +0700

    10% progress

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "book store",
+});
+
+app.use(express.json);
+app.get("/", (req, res) => {
+  res.json("hello ini backend");
+});
+
+// mengambil semua data huku
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Server is runing");
+});
